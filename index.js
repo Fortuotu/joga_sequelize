6540ff1ff5f6135d9cee145dd6b2d207943876e4
Fortuotu <ott.saluvere@gmail.com>
2026-04-08 01:31:15 -0400

    create connection to database

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 
 const port = 3000;
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("mysql://app:app_pw@localhost:3306/app_db");
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch(err => {
+        console.log('Unable to connect to the database', err);
+    });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
